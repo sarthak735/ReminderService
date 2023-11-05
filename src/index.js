570dbe91dfc047b3357d793ef520f759c2dc23f1
Kunwar Sarthak Singh <kuwarsarthaksingh735@gmail.com>
2023-11-05 20:23:34 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -3,22 +3,29 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
 
-const {sendBasicEmail} = require('./services/email-service');
+// const {sendBasicEmail} = require('./services/email-service');
+
+const TicketController = require('./controllers/ticket-controller')
+
+const jobs = require('./utils/job');
 
 const setupAndStartServer = () =>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.post('/api/v1/tickets', TicketController.create);
+    
     app.listen(PORT, () =>{
         console.log(`Server Started at port, ${PORT}`);
+        jobs();
 
-        sendBasicEmail(
-            'planmyflightss@gmail.com',
-            'kuwarsarthaksingh735@gmail.com',
-            'This is a testing mail',
-            'Hey, How aare you, I hope you like the support'
-        )
+        // sendBasicEmail(
+        //     'planmyflightss@gmail.com',
+        //     'kuwarsarthaksingh735@gmail.com',
+        //     'This is a testing mail',
+        //     'Hey, How aare you, I hope you like the support'
+        // )
     })
 }
 
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,15 +1,57 @@
 const sender = require('../config/emailConfig');
+const TicketRepository = require('../repository/ticket-repository');
 
-const sendBasicEmail = (mailFrom, mailTo, mailSubject, mailBody) => {
-    sender.sendMail({
-        from: mailFrom,
-        to: mailTo,
-        subject: mailSubject,
-        text: mailBody
-    })
+const repo = new TicketRepository();
 
+
+const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
+    try {
+        const response = await sender.sendMail({
+            from: mailFrom,
+            to: mailTo,
+            subject: mailSubject,
+            text: mailBody
+        })
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
+
+const fetchPendingEmails = async (timestamp) => {
+    try {
+        const response = await repo.get({status: "PENDING"});
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const createNotification = async (data) => {
+    try {
+        const response = await repo.create(data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const updateTicket = async (ticketId, data) => {
+    try {
+        const response = await repo.update(ticketId, data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
 module.exports = { 
-    sendBasicEmail
+    sendBasicEmail,
+    fetchPendingEmails,
+    createNotification,
+    updateTicket
 }
