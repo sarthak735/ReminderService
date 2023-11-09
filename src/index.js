b2aaf4e9475c01812c445a893f86c8cd99ee66a6
Kunwar Sarthak Singh <kuwarsarthaksingh735@gmail.com>
2023-11-09 14:58:08 +0530

    Added logic for Subscribing the message from message Queues

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {PORT} = require('./config/serverConfig');
+const {PORT, REMINDER_BINDING_KEY} = require('./config/serverConfig');
+const{createChannel, subscribeMessage} = require('./utils/messageQueues')
 
 // const {sendBasicEmail} = require('./services/email-service');
 
-const TicketController = require('./controllers/ticket-controller')
+const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
 
-const jobs = require('./utils/job');
+// const jobs = require('./utils/job');
 
-const setupAndStartServer = () =>{
+const setupAndStartServer = async () =>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+
+
     app.post('/api/v1/tickets', TicketController.create);
+
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
     
     app.listen(PORT, () =>{
         console.log(`Server Started at port, ${PORT}`);
-        jobs();
+        //jobs();
 
         // sendBasicEmail(
         //     'planmyflightss@gmail.com',
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -46,6 +46,22 @@ const updateTicket = async (ticketId, data) => {
     }
 }
 
+const subscribeEvents = async (payload) => {
+    let service = payload.service;
+    let data = payload.data;
+    switch(service){
+        case 'CREATE_TICKET':
+            await createNotification(data);
+            break;
+        case 'SEND_BASIC_EMAIL':
+            await sendBasicEmail(data);
+            break
+        default:
+            console.log("No valid event received");
+            break;    
+    }
+}
+
 
 
 
@@ -53,5 +69,6 @@ module.exports = {
     sendBasicEmail,
     fetchPendingEmails,
     createNotification,
-    updateTicket
+    updateTicket,
+    subscribeEvents
 }
